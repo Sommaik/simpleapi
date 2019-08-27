83b046f851f2077bd11cd5ccc7bb452020d9ab38
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-08-27 10:05:51 +0700

    add update

--- app/controller/customer.js
+++ app/controller/customer.js
@@ -32,41 +32,48 @@ router.post('', (req, res) => {
         '${req.body.phone}'
     )
     `
-  waterfall(
-    [
-      step1,
-      (con, cb) => {
-        con.query(sql, (err, result) => {
-          pool.releaseConnection(con)
-          cb(err, result)
-        })
-      }
-    ],
-    (err, result) => {
-      if (err) {
-        res.send(err)
-      } else {
-        res.json(result)
-      }
+  mydb.executeSql(sql, (err, result) => {
+    if (err) {
+      res.end(err)
+    } else {
+      res.json(result)
     }
-  )
+  })
 })
 
-// router.get('', (req, res) => {
-//   const sql = 'select * from customers'
-//   pool.getConnection((err, con) => {
-//     if (err) {
-//       res.send(err)
-//     } else {
-//       con.query(sql, (err, result) => {
-//         if (err) {
-//           res.send(err)
-//         } else {
-//           res.json(result)
-//         }
-//       })
-//     }
-//   })
-// })
+router.delete('/:customerId', (req, res) => {
+  const sql = `
+  delete from customers
+  where customer_id = ${req.params.customerId}
+  `
+  mydb.executeSql(sql, (err, result) => {
+    if (err) {
+      res.end(err)
+    } else {
+      res.json(result)
+    }
+  })
+})
+
+router.put('/:customerId', (req, res) => {
+  const sql = `
+  update customers
+  set customer_first_name = '${req.body.firstName}',
+  customer_last_name = '${req.body.lastName}',
+  customer_address = '${req.body.address}',
+  customer_city = '${req.body.city}',
+  customer_state = '${req.body.state}',
+  customer_zip = '${req.body.zip}',
+  customer_phone = '${req.body.phone}'
+  where customer_id = ${req.params.customerId}
+  `
+  mydb.executeSql(sql, (err, result) => {
+    if (err) {
+      res.end(err)
+    } else {
+      res.json(result)
+    }
+  })
+})
 
 module.exports = router
